2d5065952bd4a9a7cfdf7b5fe0b774869330e84c
kirik <alkirichenko81@gmail.com>
2021-08-09 11:19:05 +0300

    add local images

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom"
 import App from "./components/app/app.jsx"
 import {Provider} from "react-redux";
 import store from "./reducers/store";
-import {HashRouter} from "react-router-dom";
+import {HashRouter, BrowserRouter} from "react-router-dom";
 
 
 ReactDOM.render(
